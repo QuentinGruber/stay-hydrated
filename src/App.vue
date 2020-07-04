f36a802de7dfb5485a44affecab5f52c1decc2a6
Quentin Gruber <quentingruber@gmail.com>
2020-07-05 00:19:23 +0200

    switch electron app to vue+electron app

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>STAY HYDRATED !</h1>
+    <WaterLevel />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import WaterLevel from "./components/WaterLevel.vue";
+
+@Component({
+  components: {
+    WaterLevel
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/WaterLevel.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <div class="glass">
+      <div class="water"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import $ from "jquery";
+
+$(function() {
+  $(".water").animate(
+    {
+      height: "75%",
+    },
+    1000
+  );
+});
+
+@Component
+export default class WaterLevel extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.glass {
+  margin: 30px auto;
+  height: 300px;
+  width: 100px;
+  position: relative;
+  border-style: none solid solid solid;
+  border-width: 10px;
+  border-color: lightgray;
+  border-radius: 10px;
+}
+
+.water {
+  width: 100%;
+  height: 10%;
+  background-color: skyblue;
+  position: absolute;
+  bottom: 0;
+}
+</style>
